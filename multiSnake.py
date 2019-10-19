583682a721a9bf51a7efeb3ede8ce20f11184bf9
Michael Heron <40605443+mikeheroncode@users.noreply.github.com>
2019-10-19 12:51:20 -0400

    Added basic game functions and finished event loop

--- multiSnake.py
+++ multiSnake.py
@@ -11,7 +11,21 @@ class multiSnake:
             self.snakeBlock = pygame.image.load('snake.png')
             self.alive = True
         def growX(self, xChange, gameDisplay):
+            if (xChange != 0 and self.alive):
+                self.gridCoords.append((self.gridCoords[-1][0]+xChange, self.gridCoords[-1][1]))
+                self.coords.append((self.coords[-1][0]+xChange*self.blockSize, self.coords[-1][1]))
+                for x, y in self.coords:
+                    gameDisplay.blit(self.snakeBlock, (x,y))
         def growY(self, yChange, gameDisplay):
+            if (yChange != 0) and self.alive:
+                self.gridCoords.append((self.gridCoords[-1][0], self.gridCoords[-1][1]+yChange))
+                self.coords.append((self.coords[-1][0], self.coords[-1][1]+yChange*self.blockSize))
+                for x, y in self.coords:
+                    gameDisplay.blit(self.snakeBlock, (x,y))
+        def kill(self):
+            self.alive = False
+            self.coords = []
+            self.gridCoords = []
     def __init__(self):
         pygame.init()
         print("start")
@@ -24,6 +38,38 @@ class multiSnake:
         pygame.display.set_caption('MultiSnake')
         self.clock = pygame.time.Clock()
         self.spaceTaken = {}
+        for i in range(int(self.displayWidth/10)):
+            self.spaceTaken[i] = {}
+        self.players = []
+        self.playerCount = 2
+        
+        self.x = (self.displayWidth * 0.2)/10
+        self.y = (self.displayHeight * 0.2)/10
+        for i in range(self.playerCount):
+            if (i == 0):
+                self.players.append(self.snake(self.displayWidth * 0.2, self.displayHeight * 0.2, self.gameDisplay, 10))
+            elif (i == 1):
+                self.players.append(self.snake(self.displayWidth * 0.8, self.displayHeight * 0.2, self.gameDisplay, 10))
+            elif (i == 2):
+                self.players.append(self.snake(self.displayWidth * 0.2, self.displayHeight * 0.8, self.gameDisplay, 10))
+            elif (i == 3):
+                self.players.append(self.snake(self.displayWidth * 0.8, self.displayHeight * 0.8, self.gameDisplay, 10))
+        
+
+        self.game_loop()
+        pygame.quit()
+        quit()
+
+    
+    def checkForCollision(self, x, y):
+        if (x in self.spaceTaken.keys()):
+            if (y in self.spaceTaken[x].keys()):
+                return True
+        return False
+        
+
+
+
     def game_loop(self):
         
 

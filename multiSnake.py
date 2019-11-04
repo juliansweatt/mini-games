4414b2af44c68ae8a6d8de84592b4379c61124d7
Michael Heron <mph12e>
2019-11-04 18:16:11 -0500

    Added joystick controls and changed last directional input to be class parameter

--- multiSnake.py
+++ multiSnake.py
@@ -2,6 +2,7 @@ from pygame.locals import *
 from random import randint
 import pygame
 import time
+
 class multiSnake:
     class snake:
         def __init__(self, x, y, gameDisplay, blockSize):
@@ -10,6 +11,8 @@ class multiSnake:
             self.blockSize = blockSize
             self.snakeBlock = pygame.image.load('snake.png')
             self.alive = True
+            self.x_change = 0
+            self.y_change = 1
         def growX(self, xChange, gameDisplay):
             if (xChange != 0 and self.alive):
                 self.gridCoords.append((self.gridCoords[-1][0]+xChange, self.gridCoords[-1][1]))
@@ -28,7 +31,6 @@ class multiSnake:
             self.gridCoords = []
     def __init__(self):
         pygame.init()
-        print("start")
         self.black = (0,0,0)
         self.white = (255,255,255)
         self.red = (255,0,0)
@@ -38,6 +40,16 @@ class multiSnake:
         pygame.display.set_caption('MultiSnake')
         self.clock = pygame.time.Clock()
         self.spaceTaken = {}
+        pygame.joystick.init()
+        joystick_count = pygame.joystick.get_count()
+        for i in range(joystick_count):
+            joystick = pygame.joystick.Joystick(i)
+            joystick.init()
+            buttons = joystick.get_numbuttons()
+            for i in range(buttons):
+                button = joystick.get_button(i)
+
+
         for i in range(int(self.displayWidth/10)):
             self.spaceTaken[i] = {}
         self.players = []
@@ -67,50 +79,57 @@ class multiSnake:
                 return True
         return False
         
-
-
-
     def game_loop(self):
-        
-
         x_change = 0
         y_change = 1
-
         gameExit = False
-
-
         while not gameExit:
-            
-            
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+            playerEvent = None
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameExit = True
                 if event.type == pygame.KEYDOWN:
+                    player = self.players[0]
                     if event.key == pygame.K_LEFT:
-                        x_change = -1
-                        y_change = 0
+                        player.x_change = -1
+                        player.y_change = 0
                     if event.key == pygame.K_RIGHT:
-                        x_change = 1
-                        y_change = 0
+                        player.x_change = 1
+                        player.y_change = 0
+                        name = pygame.joystick.Joystick(0).get_name()
                     if event.key == pygame.K_DOWN:
-                        x_change = 0
-                        y_change = 1
+                        player.x_change = 0
+                        player.y_change = 1
                     if event.key == pygame.K_UP:
-                        x_change = 0
-                        y_change = -1
-            self.gameDisplay.fill(self.black)
+                        player.x_change = 0
+                        player.y_change = -1
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    player = self.players[1]
+                    if event.button == 2:
+                        player.x_change = -1
+                        player.y_change = 0
+                    if event.button == 1:
+                        player.x_change = 1
+                        player.y_change = 0
+                        name = pygame.joystick.Joystick(0).get_name()
+                    if event.button == 0:
+                        player.x_change = 0
+                        player.y_change = 1
+                    if event.button == 3:
+                        player.x_change = 0
+                        player.y_change = -1
             for player in self.players:
-                if (not player.alive):
-                    continue
-                self.x = int(player.gridCoords[-1][0] + x_change)
-                self.y = int(player.gridCoords[-1][1] + y_change)
+                self.x = int(player.gridCoords[-1][0] + player.x_change)
+                self.y = int(player.gridCoords[-1][1] + player.y_change)
                 if(self.checkForCollision(self.x, self.y)):
                     for x, y in player.gridCoords:
                         del self.spaceTaken[x][y]
                     player.kill()
                     print("COLLISION: ", self.x, self.y)
-                player.growX(x_change, self.gameDisplay)
-                player.growY(y_change, self.gameDisplay)
+                player.growX(player.x_change, self.gameDisplay)
+                player.growY(player.y_change, self.gameDisplay)
                 try:
                     self.spaceTaken[self.x][self.y] = 1
                 except KeyError:
@@ -119,6 +138,8 @@ class multiSnake:
                         for y in self.spaceTaken[x]:
                             print(x, y)
                     quit()
+            
+            
             pygame.display.update()
             self.clock.tick(20)
 
@@ -127,5 +148,4 @@ class multiSnake:
     
  
 if __name__ == "__main__" :
-    print("FIRST")
     game = multiSnake()

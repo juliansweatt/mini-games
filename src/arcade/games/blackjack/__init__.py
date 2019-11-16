a46a44d3a6a63710e88acad2844ad728e78a9fe1
Michael Heron <mph12e>
2019-11-15 19:09:59 -0500

    Initial Commit for Blackjack

---
+++ src/arcade/games/blackjack/__init__.py
@@ -0,0 +1,4 @@
+from arcade.games.blackjack import blackjack
+
+def insert_cartridge():
+    return blackjack.Game()
---
+++ src/arcade/games/blackjack/blackjack.py
@@ -0,0 +1,115 @@
+import pygame
+import os
+from arcade import plethoraAPI
+from enum import IntFlag, auto, unique
+
+from pygame.locals import (
+    QUIT,
+    K_SPACE,
+    K_UP, K_DOWN, K_LEFT, K_RIGHT,
+    K_q,
+    KEYDOWN, KEYUP,
+    MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN,
+)
+
+from typing import Tuple
+
+@unique
+class ArrowMask(IntFlag):
+    """ Enum used to mask arrow keys (up, right, down, left) """
+    up = auto()
+    right = auto()
+    down = auto()
+    left = auto()
+import pygame
+import os
+from arcade import plethoraAPI
+from enum import IntFlag, auto, unique
+
+from pygame.locals import (
+    QUIT,
+    K_SPACE,
+    K_UP, K_DOWN, K_LEFT, K_RIGHT,
+    K_q,
+    KEYDOWN, KEYUP,
+    MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN,
+)
+
+from typing import Tuple
+
+@unique
+class ArrowMask(IntFlag):
+    """ Enum used to mask arrow keys (up, right, down, left) """
+    up = auto()
+    right = auto()
+    down = auto()
+    left = auto()
+
+import random
+class Game(plethoraAPI.Game):
+    def onevent(self, event: pygame.event):
+        """ called from :func:`PlethoraAPI.mainloop` when there is an event while this game is running
+        Args:
+            event: a pygame.event fetched from :func:`pygame.event.get` in
+                :func:`arcade.plethoraAPI.PlethoraAPI.mainloop`
+        Returns:
+            bool: True if onrender should be called on the next frame; False otherwise
+        """
+        if event.type == KEYDOWN:
+            # if arrow keydown:
+            #   1) add to key mask to `self.arrows`
+            #   2) add opposite key mask to `self.arrows_hidden`
+            #   3) remove key mask from `self.arrows_hidden`
+            if event.key == pygame.K_UP:
+                self.arrows |= ArrowMask.up
+                self.arrows_hidden |= ArrowMask.down
+                self.arrows_hidden &= ~ArrowMask.up
+            elif event.key == pygame.K_RIGHT:
+                self.arrows |= ArrowMask.right
+                self.arrows_hidden |= ArrowMask.left
+                self.arrows_hidden &= ~ArrowMask.right
+            elif event.key == pygame.K_DOWN:
+                self.arrows |= ArrowMask.down
+                self.arrows_hidden |= ArrowMask.up
+                self.arrows_hidden &= ~ArrowMask.down
+            elif event.key == pygame.K_LEFT:
+                self.arrows |= ArrowMask.left
+                self.arrows_hidden |= ArrowMask.right
+                self.arrows_hidden &= ~ArrowMask.left
+            else:
+                return False
+            return True
+        if event.type == KEYUP:
+            # if arrow keyup:
+            #   1) remove key mask from `self.arrows`
+            #   2) remove both key and opposite key from `self.arrows_hidden`
+            if event.key == pygame.K_UP:
+                self.arrows &= ~ArrowMask.up
+                self.arrows_hidden &= ~(ArrowMask.up | ArrowMask.down)
+            elif event.key == pygame.K_RIGHT:
+                self.arrows &= ~ArrowMask.right
+                self.arrows_hidden &= ~(ArrowMask.left | ArrowMask.right)
+            elif event.key == pygame.K_DOWN:
+                self.arrows &= ~ArrowMask.down
+                self.arrows_hidden &= ~(ArrowMask.up | ArrowMask.down)
+            elif event.key == pygame.K_LEFT:
+                self.arrows &= ~ArrowMask.left
+                self.arrows_hidden &= ~(ArrowMask.left | ArrowMask.right)
+            else:
+                return False
+            return True
+        return False
+    
+    def onrender(self) -> bool:
+        """ called from :func:`PlethoraAPI.mainloop` when game is dirty
+        The game is dirty when:
+            - The game is first loaded
+            - True is returned from :func:`Game.onevent`
+            - True is returned from :func:`Game.onrender`
+        Returns:
+            bool: True if onrender should be called again on the next frame - this is useful if a
+                  key is down and re-rendering should occur; False otherwise
+        """
+
+import random
+class Game(plethoraAPI.Game):

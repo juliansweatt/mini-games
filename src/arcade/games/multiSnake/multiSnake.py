6a68e23f4ee498df5075440be965608b0e0bee0c
Michael Heron <mph12e>
2019-11-30 13:38:59 -0500

    Remove duplicate code form onevent and fixed bug with return statement

--- src/arcade/games/multiSnake/multiSnake.py
+++ src/arcade/games/multiSnake/multiSnake.py
@@ -59,8 +59,6 @@ class Game(plethoraAPI.Game):
                 button = joystick.get_button(i)
 
 
-        for i in range(int(self.rect.width/10)):
-            self.spaceTaken[i] = {}
         self.players = []
         self.playerCount = 2
         
@@ -85,69 +83,51 @@ class Game(plethoraAPI.Game):
         if ((x,y) in self.spaceTaken):
             return True
         return False
-    def onevent(self, event):
-        x_change = 0
-        y_change = 1
-        gameExit = False
-        while not gameExit:
-            joystick = pygame.joystick.Joystick(0)
-            joystick.init()
-            playerEvent = None
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    gameExit = True
-                if event.type == pygame.KEYDOWN:
-                    player = self.players[0]
-                    if event.key == pygame.K_LEFT:
-                        player.x_change = -1
-                        player.y_change = 0
-                    if event.key == pygame.K_RIGHT:
-                        player.x_change = 1
-                        player.y_change = 0
-                        name = pygame.joystick.Joystick(0).get_name()
-                    if event.key == pygame.K_DOWN:
-                        player.x_change = 0
-                        player.y_change = 1
-                    if event.key == pygame.K_UP:
-                        player.x_change = 0
-                        player.y_change = -1
-                elif event.type == pygame.JOYBUTTONDOWN:
-                    player = self.players[1]
-                    if event.button == 2:
-                        player.x_change = -1
-                        player.y_change = 0
-                    if event.button == 1:
-                        player.x_change = 1
-                        player.y_change = 0
-                        name = pygame.joystick.Joystick(0).get_name()
-                    if event.button == 0:
-                        player.x_change = 0
-                        player.y_change = 1
-                    if event.button == 3:
-                        player.x_change = 0
-                        player.y_change = -1
-            for player in self.players:
-                self.x = int(player.gridCoords[-1][0] + player.x_change)
-                self.y = int(player.gridCoords[-1][1] + player.y_change)
-                if(self.checkForCollision(self.x, self.y)):
-                    for x, y in player.gridCoords:
-                        del self.spaceTaken[x][y]
-                    player.kill()
-                    print("COLLISION: ", self.x, self.y)
-                player.growX = True
-                player.growY = True(player.y_change, self.display)
-                try:
-                    self.spaceTaken[self.x][self.y] = 1
-                except KeyError:
-                    print("Dead")
-                    for x in self.spaceTaken:
-                        for y in self.spaceTaken[x]:
-                            print(x, y)
-                    quit()
-            
-            
-            pygame.display.update()
-            self.clock.tick(20)
+    
+    def onevent(self, event: pygame.event):
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+        if event.type == pygame.QUIT:
+            gameExit = True
+        if event.type == pygame.KEYDOWN:
+            player = self.players[0]
+            if event.key == pygame.K_LEFT:
+                player.x_change = -1
+                player.y_change = 0
+            if event.key == pygame.K_RIGHT:
+                player.x_change = 1
+                player.y_change = 0
+                name = pygame.joystick.Joystick(0).get_name()
+            if event.key == pygame.K_DOWN:
+                player.x_change = 0
+                player.y_change = 1
+            if event.key == pygame.K_UP:
+                player.x_change = 0
+                player.y_change = -1
+            self.render = True
+        elif event.type == pygame.JOYBUTTONDOWN:
+            player = self.players[1]
+            if event.button == 2:
+                player.x_change = -1
+                player.y_change = 0
+            if event.button == 1:
+                player.x_change = 1
+                player.y_change = 0
+                name = pygame.joystick.Joystick(0).get_name()
+            if event.button == 0:
+                player.x_change = 0
+                player.y_change = 1
+            if event.button == 3:
+                player.x_change = 0
+                player.y_change = -1
+            self.render = True
+        
+        if (self.render):
+            return True
+        else:
+            return False
+        
+        
 
 
     def onrender(self):
